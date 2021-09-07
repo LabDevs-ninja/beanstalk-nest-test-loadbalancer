bae18760d99c58b4764b15c131a3791e71657845
José Menezes <jcarlos78@gmail.com>
2021-09-07 11:54:08 -0300

    nest build

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 const { PORT = 3000 } = process.env;
 //process.env.PORT from AWS beanstalk
+console.log('PORT:', PORT);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
