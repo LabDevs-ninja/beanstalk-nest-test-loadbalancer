32c7cdc595716cbf2181d5884ba4d2de21df70ad
José Menezes <jcarlos78@gmail.com>
2021-09-07 15:10:14 -0300

    terminus health-check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthcheckController } from './healthcheck/healthcheck.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TerminusModule, HttpModule],
+  controllers: [AppController, HealthcheckController],
   providers: [AppService],
 })
 export class AppModule {}
